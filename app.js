8479b8a29c9c007d5ced58172eec09e3791e0434
chrismgonzalez <cmgonza89@gmail.com>
2019-09-25 14:55:12 -0500

    added affirmations

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const authToken = process.env.TWILIO_ACCOUNT_AUTHTOKEN;
 const client = new twilio(accountSid, authToken);
 const cronJob = require("cron").CronJob;
 
+/* //todo:
+call a function to generate the body of the text message from an array of messages
+
+use the return value of the function as the template literal value for the body of the text message
+
+
+
+*/
+
 let sendText = new cronJob(
   "45 14 * * *",
   function() {
---
+++ sweetnothings.js
@@ -0,0 +1,6 @@
+const affirmations = [
+  "You are beautiful",
+  "You look beautiful today",
+  "I love you so much!",
+  "You are my heart and soul"
+];
